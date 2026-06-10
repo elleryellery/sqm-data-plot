28d6aae189d3262a7e8ffe30360ab4f4d1c486a4
elleryellery <ellerymcd@gmail.com>
2026-06-10 01:47:31 -0400

    Initial Commit: Added two example data files. Initial features include: selecting file from file explorer, parsing .dat file, plotting MSAS vs. time from file

---
+++ parse.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import datetime
+from tkinter.filedialog import askopenfilename
+
+filename = askopenfilename(filetypes=[("DAT File", "*.dat")])
+
+# File format:
+#      UTC Date & Time, Local Date & Time, Temperature, Counts, Frequency, MSAS
+#      YYYY-MM-DDTHH:mm:ss.fff;YYYY-MM-DDTHH:mm:ss.fff;Celsius;number;Hz;mag/arcsec^2
+
+time_utc = [] # UTC Date & Time // YYYY-MM-DDTHH:mm:ss.fff
+time_local = [] # Local Date & Time // YYYY-MM-DDTHH:mm:ss.fff
+temp = [] # Temperature // Celsius
+count = [] # Counts // no units
+freq = [] # Frequence // Hz
+msas = [] # MSAS // mag/arcsec^2
+
+with open(filename) as datfile:
+    for line in datfile:
+        if(line[0] == '#'): # Skip comment lines
+            continue
+
+        data = line.split(';') # Split values as specified in file format
+        
+        time_utc.append(data[0])
+        time_local.append(data[1])
+        temp.append(data[2])
+        count.append(data[3])
+        freq.append(data[4])
+        msas.append(float(data[5].replace('\n', ''))) # Remove new line indicator from end of line and interpret value as a float
+
+def format(times): # Format UTC or local times as datetime objects so that they can be interpreted and plotted by matplotlib
+    out = []
+
+    for point in times:
+        date, time = point.split('T')
+        year, month, day = date.split('-')
+        hour, min, sec = time.split(':')
+
+        out.append(datetime.datetime(int(year), int(month), int(day), int(hour), int(min)))
+
+    return out
+
+plt.figure()
+plt.plot(format(time_local), msas) # Plot time on x-axis, MSAS on y-axis
+
+plt.gca().invert_yaxis() # Flip y-axis upside down as specified by Tim
+
+plt.xlabel("Time (s)")
+plt.ylabel("MSAS")
+plt.title("MSAS vs Time")
+plt.grid()
+
+plt.show()
